dbc2d90a2e2e12e429c749734b977e4cdae25b07
Leandro Da Souza <dasouza.leandro@gmail.com>
2026-09-02 17:58:49 +0200

    add login page and route, add basic scss system

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: Login,
+    title: 'Login',
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+];
---
+++ apps/web/src/app/features/auth/login/login.ts
@@ -0,0 +1,27 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  imports: [ReactiveFormsModule],
+  selector: 'app-login',
+  styleUrl: './login.scss',
+  templateUrl: './login.html',
+})
+export class Login {
+  private readonly formBuilder = inject(FormBuilder);
+
+  protected readonly loginForm = this.formBuilder.nonNullable.group({
+    email: ['', [Validators.email, Validators.required]],
+    password: ['', Validators.required],
+  });
+
+  protected onSubmit(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAsTouched();
+      return;
+    }
+
+    const credentials = this.loginForm.getRawValue();
+    console.log(credentials);
+  }
+}
