5e78befa91ca7874d268a036012bd07115bcae25
Leandro Da Souza <dasouza.leandro@gmail.com>
2026-09-08 13:00:42 +0200

    add auth layout template and navbar

---
+++ apps/web/src/app/layouts/authenticated-layout/authenticated-layout/authenticated-layout.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-authenticated-layout',
+  styleUrl: './authenticated-layout.scss',
+  templateUrl: './authenticated-layout.html',
+})
+export class AuthenticatedLayout {}
