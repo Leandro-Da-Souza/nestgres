fddd45c55bd8c284292c737ba5c9d13ed583c3bb
Leandro Da Souza <dasouza.leandro@gmail.com>
2026-09-24 19:34:24 +0200

    add invoice api req

--- apps/web/src/app/features/invoices/invoice.service.ts
+++ apps/web/src/app/features/invoices/invoice.service.ts
@@ -1,4 +1,13 @@
-import { Service } from '@angular/core';
+import { inject, Service } from '@angular/core';
+import { ApiResponse, Invoice } from '@nestgres/contracts';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Service()
-export class InvoiceService {}
+export class InvoiceService {
+  private readonly http = inject(HttpClient);
+
+  public getInvoices(): Observable<ApiResponse<Invoice[]>> {
+    return this.http.get<ApiResponse<Invoice[]>>('api/invoices');
+  }
+}
--- apps/web/src/app/features/invoices/pages/invoices/invoices.ts
+++ apps/web/src/app/features/invoices/pages/invoices/invoices.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, Signal } from '@angular/core';
+import { InvoiceService } from '../../invoice.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
+import { Invoice } from '@nestgres/contracts';
+import { JsonPipe } from '@angular/common';
 
 @Component({
-  imports: [],
+  imports: [JsonPipe],
   selector: 'app-invoices',
   styleUrl: './invoices.scss',
   templateUrl: './invoices.html',
 })
-export class Invoices {}
+export class Invoices {
+  private readonly invoiceService = inject(InvoiceService);
+
+  public readonly invoices: Signal<Invoice[]> = toSignal(
+    this.invoiceService.getInvoices().pipe(map((invoice) => invoice.data)),
+    { initialValue: [] },
+  );
+}
